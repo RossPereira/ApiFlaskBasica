f5afb1e6f03e88a86b7d05d254fc042cd8b2b910
Ross Pereira <devrodolfopereira@gmail.com>
2022-03-01 11:59:07 -0300

    alteracao app

--- api.py
+++ api.py
@@ -11,6 +11,14 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 def index():
     return "<h1>Hello World</h1>"
 
+
+@app.route("/somar/<int:n1>/<int:n2>", methods=['GET'])
+@cross_origin()
+def somar(n1, n2):
+    resultado = n1 + n2
+    resultado = str(resultado)
+    return resultado
+
 def main():
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
