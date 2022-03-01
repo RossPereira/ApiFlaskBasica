d5661e123bb61e127169683176e6b5edcd44cff7
Ross Pereira <devrodolfopereira@gmail.com>
2022-03-01 11:07:05 -0300

    primeiro commit

---
+++ api.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route("/", methods=['GET'])
+@cross_origin()
+def index():
+    return "<h1>Hello World</h1>"
+
+def main():
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    main()
